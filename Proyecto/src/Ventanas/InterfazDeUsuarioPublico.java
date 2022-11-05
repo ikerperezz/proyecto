86cbad420716d4da62a5a12817cbb3dbace8a0c0
Josu Igoa <josu.igoa@opendeusto.es>
2022-11-05 22:39:50 +0100

    Diseño y estructura principal de la página de inicio

--- Proyecto/src/Ventanas/InterfazDeUsuarioPublico.java
+++ Proyecto/src/Ventanas/InterfazDeUsuarioPublico.java
@@ -6,6 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Clases.Usuario;
+import Clases.UsuarioPublico;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
 public class InterfazDeUsuarioPublico extends JFrame {
 
 	private JPanel contentPane;
@@ -14,13 +23,52 @@ public class InterfazDeUsuarioPublico extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public InterfazDeUsuarioPublico() {
+	public InterfazDeUsuarioPublico(Usuario usuario, UsuarioPublico idLiga) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 491, 401);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel labelBienvenida = new JLabel("Bienvenido "+ usuario.getUsuario());
+		labelBienvenida.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelBienvenida.setBounds(69, 162, 326, 59);
+		contentPane.add(labelBienvenida);
+		
+		JLabel labelNombreLiga = new JLabel("Liga con ID " + idLiga.getIdLiga());
+		labelNombreLiga.setFont(new Font("Tahoma", Font.BOLD, 20));
+		labelNombreLiga.setBounds(69, 73, 330, 59);
+		contentPane.add(labelNombreLiga);
+		
+		JButton botonInicio = new JButton("INICIO");
+		botonInicio.setFont(new Font("Tahoma", Font.BOLD, 14));
+		botonInicio.setBounds(0, 301, 89, 63);
+		contentPane.add(botonInicio);
+		
+		JButton botonClasificacion = new JButton("CLASIFICACION");
+		botonClasificacion.setBackground(new Color(255, 175, 175));
+		botonClasificacion.setFont(new Font("Tahoma", Font.BOLD, 11));
+		botonClasificacion.setBounds(99, 301, 125, 63);
+		contentPane.add(botonClasificacion);
+		
+		JButton botonEquipo = new JButton("EQUIPO");
+		botonEquipo.setBackground(Color.YELLOW);
+		botonEquipo.setFont(new Font("Tahoma", Font.BOLD, 16));
+		botonEquipo.setBounds(234, 301, 107, 63);
+		contentPane.add(botonEquipo);
+		
+		JButton botonMercado = new JButton("MERCADO");
+		botonMercado.setBackground(Color.GREEN);
+		botonMercado.setFont(new Font("Tahoma", Font.BOLD, 15));
+		botonMercado.setBounds(348, 301, 122, 63);
+		contentPane.add(botonMercado);
+		
+		JButton botonAjustes = new JButton("AJUSTES");
+		botonAjustes.setBackground(Color.RED);
+		botonAjustes.setFont(new Font("Tahoma", Font.BOLD, 15));
+		botonAjustes.setBounds(370, 0, 107, 63);
+		contentPane.add(botonAjustes);
 	}
-
 }
